a50e3f5eccb832551de0965f2111f74bb34bf76a
Bruno Vieira <bruno.vieira@lynkco.com>
2026-04-05 10:42:32 +0200

    + Remember ViewModel across recompositions

--- app/src/main/java/com/challenge/moises/AppNavHost.kt
+++ app/src/main/java/com/challenge/moises/AppNavHost.kt
@@ -4,7 +4,9 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
+import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
 import androidx.navigation3.runtime.NavEntry
+import androidx.navigation3.runtime.rememberSaveableStateHolderNavEntryDecorator
 import androidx.navigation3.ui.NavDisplay
 import com.challenge.moises.common.navigation.NavKey
 import com.challenge.moises.feature.album.ui.screens.AlbumScreen
@@ -23,6 +25,10 @@ fun AppNavHost() {
 
     NavDisplay(
         backStack = backStack,
+        entryDecorators = listOf(
+            rememberSaveableStateHolderNavEntryDecorator(),
+            rememberViewModelStoreNavEntryDecorator()
+        ),
         onBack = { 
             if (backStack.size > 1) backStack.removeAt(backStack.size - 1) 
         }
