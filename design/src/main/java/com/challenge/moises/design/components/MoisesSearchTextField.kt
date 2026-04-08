1bbfd8480847b739a6029f1a5b13ab9d53eb65be
Bruno Vieira <bruno.vieira@lynkco.com>
2026-04-08 23:36:02 +0200

    + Extract SearchTextField into a reusable component

---
+++ design/src/main/java/com/challenge/moises/design/components/MoisesSearchTextField.kt
@@ -0,0 +1,49 @@
+package com.challenge.moises.design.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import com.challenge.moises.design.R
+
+@Composable
+fun MoisesSearchTextField(
+    modifier: Modifier = Modifier,
+    query: String,
+    onQueryChanged: (String) -> Unit,
+    onSearch: () -> Unit = {}
+) {
+    TextField(
+        modifier = modifier.fillMaxWidth(),
+        value = query,
+        onValueChange = onQueryChanged,
+        placeholder = {
+            Text(
+                stringResource(R.string.search_hint),
+                color = Color.Gray
+            )
+        },
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = Color.Transparent,
+            unfocusedContainerColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            cursorColor = Color.White,
+            focusedTextColor = Color.White
+        ),
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+        keyboardActions = KeyboardActions(
+            onSearch = {
+                onSearch()
+            }
+        )
+    )
+}
