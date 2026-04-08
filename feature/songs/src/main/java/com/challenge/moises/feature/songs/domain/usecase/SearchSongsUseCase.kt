3f1173784831b7b4ea9a7af9173edde86edd1770
Bruno Vieira <bruno.vieira@lynkco.com>
2026-04-08 23:37:01 +0200

    + Optmize imports

--- feature/songs/src/main/java/com/challenge/moises/feature/songs/domain/usecase/SearchSongsUseCase.kt
+++ feature/songs/src/main/java/com/challenge/moises/feature/songs/domain/usecase/SearchSongsUseCase.kt
@@ -1,16 +1,14 @@
 package com.challenge.moises.feature.songs.domain.usecase
 
 import com.challenge.moises.core.network.data.repositories.ItunesRepository
-import com.challenge.moises.core.network.domain.models.Song
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
 import javax.inject.Inject
 
 class SearchSongsUseCase @Inject constructor(
     private val repository: ItunesRepository
 ) {
-    operator fun invoke(query: String): Flow<List<Song>> {
-        if (query.isBlank()) return emptyFlow()
-        return repository.searchSongs(query)
+    operator fun invoke(query: String) = run {
+        if (query.isBlank()) emptyFlow()
+        else repository.searchSongs(query)
     }
 }
