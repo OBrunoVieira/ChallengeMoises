9069afecc1be655113e89def86f3802e50591444
Bruno Vieira <bruno.vieira@lynkco.com>
2026-04-09 00:57:08 +0200

    + Update ITunesApi with default query parameters and add songDetails and album lookups

--- core/network/src/main/java/com/challenge/moises/core/network/ITunesApi.kt
+++ core/network/src/main/java/com/challenge/moises/core/network/ITunesApi.kt
@@ -8,13 +8,19 @@ interface ITunesApi {
     @GET("search")
     suspend fun search(
         @Query("term") term: String,
-        @Query("entity") entity: String? = null,
-        @Query("limit") limit: Int = 50
+        @Query("entity") entity: String? = "musicTrack",
+        @Query("limit") limit: Int = 20,
     ): ItunesSearchResponseDTO
 
     @GET("lookup")
-    suspend fun lookup(
+    suspend fun songDetails(
         @Query("id") id: String,
-        @Query("entity") entity: String? = null
+        @Query("entity") entity: String? = "musicTrack"
+    ): ItunesSearchResponseDTO
+
+    @GET("lookup")
+    suspend fun album(
+        @Query("id") id: String,
+        @Query("entity") entity: String? = "song"
     ): ItunesSearchResponseDTO
 }
